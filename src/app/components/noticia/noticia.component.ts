0f83c4dfed5b0437a12b78a473058b851943e02a
Susana Santos Moreno <susiylola@gmail.com>
2019-04-27 18:34:20 +0200

    Componentes Noticias y Noticia creados y funcionando.

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-noticia',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticia.component.scss'],
 })
 export class NoticiaComponent implements OnInit {
+
+  @Input() noticia: Article;
+  @Input() indice: number;
 
   constructor() { }
 
